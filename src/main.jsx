db45dc0f71f41a0cf7730d8d24c039cfe9c732fe
Yousof999 <yousofalaa26@gmail.com>
2026-08-28 10:03:18 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+)
